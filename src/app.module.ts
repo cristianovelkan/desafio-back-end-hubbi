7fc77b24459f2edab3a06513931c16702e953474
Cristiano Mendes <cristianovelkan@gmail.com>
2025-03-11 14:15:53 -0300

    added products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infra/env-config/env-config.module';
+import { ProductsModule } from './products/infrastructure/products.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
