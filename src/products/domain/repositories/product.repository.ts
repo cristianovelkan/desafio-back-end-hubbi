3f9b7af20d129a421c1d7ba8030e7418f7dd8fca
Cristiano Mendes <cristianovelkan@gmail.com>
2025-03-13 11:24:56 -0300

    refactoring ProductRepository

--- src/products/domain/repositories/product.repository.ts
+++ src/products/domain/repositories/product.repository.ts
@@ -1,7 +1,28 @@
 import { ProductEntity } from '../entities/product.entity'
-import { SearchableRepositoryInterface } from '@/shared/domain/repositories/searchable-repository-contract'
-export interface ProductRepository
-  extends SearchableRepositoryInterface<ProductEntity, any, any> {
-  findBySku(sku: string): Promise<ProductEntity>
-  skuExists(sku: string): Promise<void>
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contract'
+
+export namespace ProductRepository {
+  export type Filter = string
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<
+    ProductEntity,
+    Filter
+  > {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      ProductEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findBySku(sku: string): Promise<ProductEntity>
+    skuExists(sku: string): Promise<void>
+  }
 }
--- src/products/infrastructure/database/in-memory/repositories/product-in-memory.repository.ts
+++ src/products/infrastructure/database/in-memory/repositories/product-in-memory.repository.ts
@@ -3,36 +3,48 @@ import { ProductEntity } from '@/products/domain/entities/product.entity'
 import { NotFoundError } from '@/shared/domain/errors/not-found-error'
 import { ConflictError } from '@/shared/domain/errors/conflict-error'
 import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository'
+import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contract'
 
 export class ProductInMemoryRepository
   extends InMemorySearchableRepository<ProductEntity>
-  implements ProductRepository
+  implements ProductRepository.Repository
 {
+  sortableFields: string[] = ['name', 'price', 'createdAt']
+
   async findBySku(sku: string): Promise<ProductEntity> {
-    return new Promise((resolve, reject) => {
-      try {
-        const entity = this.items.find(item => item.sku === sku)
-        if (!entity) {
-          throw new NotFoundError(`Entity not found using sku ${sku}`)
-        }
-        resolve(entity)
-      } catch (error) {
-        reject(Error(error))
-      }
-    })
+    const entity = this.items.find(item => item.sku === sku)
+    if (!entity) {
+      throw new NotFoundError(`Entity not found using sku ${sku}`)
+    }
+    return entity
   }
 
   async skuExists(sku: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      try {
-        const entity = this.items.find(item => item.sku === sku)
-        if (entity) {
-          throw new ConflictError('Sku already used')
-        }
-        resolve()
-      } catch (error) {
-        reject(Error(error))
-      }
+    const entity = this.items.find(item => item.sku === sku)
+    if (entity) {
+      throw new ConflictError('Sku already used')
+    }
+  }
+
+  protected async applyFilter(
+    items: ProductEntity[],
+    filter: ProductRepository.Filter,
+  ): Promise<ProductEntity[]> {
+    if (!filter) {
+      return items
+    }
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
     })
+  }
+
+  protected async applySort(
+    items: ProductEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<ProductEntity[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDir)
   }
 }
