48109a26129acbe381b538cae4ffd8c67a80bf66
Cristiano Mendes <cristianovelkan@gmail.com>
2025-03-14 06:40:55 -0300

    Added Prisma Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { ProductsModule } from './products/infrastructure/products.module'
+import { DatabaseModule } from './shared/infrastructure/database/database.module'
 
 @Module({
-  imports: [EnvConfigModule, ProductsModule],
+  imports: [EnvConfigModule, ProductsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
