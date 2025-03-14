a1bf7dc71e262ab2083ab8799436d177ad654253
Cristiano Mendes <cristianovelkan@gmail.com>
2025-03-14 06:19:29 -0300

    ProductsController unit tests

---
+++ src/products/infrastructure/__tests__/unit/products.controller.spec.ts
@@ -0,0 +1,115 @@
+import { ProductsController } from '../../products.controller'
+import { ProductOutput } from '@/products/application/dtos/product-output'
+import { CreateProductUseCase } from '@/products/application/usecases/create-product.usecase'
+import { CreateProductDto } from '../../dtos/create-product.dto'
+import { UpdateProductUseCase } from '@/products/application/usecases/update-product.usecase'
+import { UpdateProductDto } from '../../dtos/update-product.dto'
+import { GetProductUseCase } from '@/products/application/usecases/get-product.usecase'
+import { ListProductsUseCase } from '@/products/application/usecases/list-products.usecase'
+
+describe('ProductsController unit tests', () => {
+  let sut: ProductsController
+  let id: string
+  let props: ProductOutput
+
+  beforeEach(() => {
+    sut = new ProductsController()
+    id = 'df96ae94-6128-486e-840c-b6f78abb4801'
+    props = {
+      id,
+      name: 'Banana',
+      price: 10,
+      stock: 10,
+      sku: 'abc123456',
+      createdAt: new Date(),
+    }
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+
+  it('should create a product', async () => {
+    const output: CreateProductUseCase.Output = props
+    const mockCreateProductUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['createproductUseCase'] = mockCreateProductUseCase as any
+    const input: CreateProductDto = {
+      name: 'Banana',
+      price: 10,
+      stock: 10,
+      sku: 'abc123456',
+    }
+    const result = await sut.create(input)
+    expect(output).toMatchObject(result)
+    expect(mockCreateProductUseCase.execute).toHaveBeenCalledWith(input)
+  })
+
+  it('should update a product', async () => {
+    const output: UpdateProductUseCase.Output = props
+    const mockUpdateProductUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['updateProductUseCase'] = mockUpdateProductUseCase as any
+    const input: UpdateProductDto = {
+      name: 'new name',
+      price: 20,
+      stock: 20,
+      sku: 'def123456',
+    }
+    const result = await sut.update(id, input)
+    expect(output).toMatchObject(result)
+    expect(mockUpdateProductUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    })
+  })
+
+  it('should delete a product', async () => {
+    const output = undefined
+    const mockDeleteProductUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['deleteProductUseCase'] = mockDeleteProductUseCase as any
+    const result = await sut.remove(id)
+    expect(output).toStrictEqual(result)
+    expect(mockDeleteProductUseCase.execute).toHaveBeenCalledWith({
+      id,
+    })
+  })
+
+  it('should gets a product', async () => {
+    const output: GetProductUseCase.Output = props
+    const mockGetProductUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['getProductUseCase'] = mockGetProductUseCase as any
+    const result = await sut.findOne(id)
+    expect(output).toStrictEqual(result)
+    expect(mockGetProductUseCase.execute).toHaveBeenCalledWith({
+      id,
+    })
+  })
+
+  it('should list products', async () => {
+    const output: ListProductsUseCase.Output = {
+      items: [props],
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 1,
+      total: 1,
+    }
+    const mockListProductsUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['listProductsUseCase'] = mockListProductsUseCase as any
+    const searchParams = {
+      page: 1,
+      perPage: 1,
+    }
+    const result = await sut.search(searchParams)
+    expect(output).toStrictEqual(result)
+    expect(mockListProductsUseCase.execute).toHaveBeenCalledWith(searchParams)
+  })
+})
